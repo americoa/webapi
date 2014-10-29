a5ac49bc77e0acef216c66478eee23b61b36ee10
americoa <americo.email@gmail.com>
2014-10-29 17:42:21 -0200

    01-Test WebApi

---
+++ LabAspWebApi/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabAspWebApi.Controllers
+{
+    public class ClientController : ApiController
+    {
+        [HttpGet]
+        public Dictionary<string, string> Test()
+        {
+            Dictionary<string, string> _test = new Dictionary<string, string>();
+            _test.Add("01", "TEST");
+            return _test;
+        }
+    }
+}
